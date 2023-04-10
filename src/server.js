2a2e52484abc9ce961183ed71bf4fa908d7d7c42
MKHAS <mkhas313@gmail.com>
2023-04-10 15:20:39 +0300

    Add a skeleton get and post, Add nanoid

--- src/server.js
+++ src/server.js
@@ -1,12 +1,49 @@
 // Require the framework and instantiate it
 const fastify = require("fastify")({ logger: true });
-
+const { nanoid } = require("nanoid");
 // Declare a route
 fastify.get("/", async (req, rep) => {
   res.send({ hello: "world" });
 });
 
-// Run the server!
+//#region GET
+fastify.get("/accounts", async (req, res) => {
+  //send back an account
+  res.send({
+    Name: "john",
+    Surname: "doe",
+    Balance: 1024,
+    Transactions: [
+      {
+        senderId: "0000",
+        recieverId: "0001",
+        amount: 512,
+        timestamp: new Date("2023-4-23T12:00:00Z"),
+      },
+      {
+        senderId: "0000",
+        recieverId: "0002",
+        amount: 256,
+        timestamp: new Date("2023-4-23T12:01:00Z"),
+      },
+    ],
+  });
+});
+//#endregion
+
+//#region POST
+fastify.post("/accounts", async (req, res) => {
+  //add a new account
+  // get new account info
+  // if initialAmount > 0 create transaction, if this fails send 400
+  // otherwise send 200
+  const newAccount = req.body;
+  newAccount.id = nanoid();
+  res.code(201).send(newAccount);
+});
+//#endregion
+
+// #region RUN
 const start = async () => {
   try {
     await fastify.listen({ port: 3000 });
